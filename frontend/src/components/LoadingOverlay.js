fa9c984f6315234f77a25e3d9deeddedb6b848b2
JuuyouSan <justintruong222@gmail.com>
2024-11-17 04:52:26 -0600

    Two new files for the loading animation

---
+++ frontend/src/components/LoadingOverlay.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./LoadingOverlay.css"; // Import its styles
+
+import { bouncy } from "ldrs";
+
+bouncy.register(); // Registers the <l-bouncy> loader globally
+
+const LoadingOverlay = ({ keyValue }) => {
+  return ReactDOM.createPortal(
+    <div className="loading-overlay">
+        <l-bouncy
+          size="55"
+          speed="1.8" 
+          color="orange" 
+        ></l-bouncy>
+      <div className="fallback-spinner"></div>
+    </div>,
+    document.body // Render outside of the component hierarchy
+  );
+};
+
+export default LoadingOverlay;
