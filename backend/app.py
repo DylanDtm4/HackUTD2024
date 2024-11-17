618619937267a6fcc6db1a8549322e5223eb78db
DylanDtm4 <dylandtm4@gmail.com>
2024-11-16 22:36:51 -0600

    for vercel

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
 from routes import api_routes
+from asgiref.wsgi import WsgiToAsgi
 
 # Load environment variables
 load_dotenv()
@@ -11,6 +12,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(api_routes, url_prefix='/api')
+asgi_app = WsgiToAsgi(app)
 
 if __name__ == "__main__":
     app.run(debug=True)
