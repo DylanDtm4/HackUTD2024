a81cf92c8db9d178dd296e681931cf0851526bd8
justi-lai <150742074+justi-lai@users.noreply.github.com>
2024-11-16 22:32:55 -0600

    Update app_math.py

--- backend/utils/app_math.py
+++ backend/utils/app_math.py
@@ -1,13 +1,25 @@
 import math
 
-def add(x, y):
-    return x + y
+def add(x):
+    sum = 0
+    for num in x:
+        sum += num
+    return sum
 
 def sub(x, y):
-    return x - y
+    sum = x
+    for num in x:
+        sum -= num
+    return sum
 
-def mult(x, y):
-    return x*y
+def mult(x):
+    result = 1
+    for num in x:
+        result *= num
+    return result
 
 def div(x, y):
-    return x/y
+    result = x
+    for num in y:
+        result /= num
+    return result
